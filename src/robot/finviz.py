301dd2df1f2cde58b6984fdd4edaee46567321ab
tigerding <zhiyuanding01@gmail.com>
2024-10-01 00:26:59 -0400

    feat: finviz partial metainfo

--- src/robot/finviz.py
+++ src/robot/finviz.py
@@ -3,7 +3,7 @@ Finviz backend implemeneted with web scraping.
 
 Author: tigerding
 Email: zhiyuanding01@gmail.com
-Version: 0.1.0
+Version: 0.2.0
 """
 
 import pandas as pd
@@ -15,8 +15,29 @@ FINVIZ_STOCK_URL = f"{FINVIZ_BASE_URL}/quote.ashx"
 
 CHROME_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
 
+
 class ElementNotFoundError(Exception):
     pass
+
+
+def parse_stock_page(ticker: str) -> BeautifulSoup:
+    """
+    Gets parsed html page contents for stock from finviz.com.
+
+    Args:
+        ticker (str): stock ticker symbol
+
+    Returns:
+       BeautifulSoup: parsed html page
+    """
+
+    response = requests.get(
+        FINVIZ_STOCK_URL,
+        {"t": ticker},
+        headers={"User-Agent": CHROME_USER_AGENT},
+    )
+    return BeautifulSoup(response.content, "lxml")
+
 
 def get_tags(ticker: str) -> pd.DataFrame:
     """
@@ -28,21 +49,66 @@ def get_tags(ticker: str) -> pd.DataFrame:
     Returns:
         pd.DataFrame: pandas DataFrame of stock tags
     """
-    
-    response = requests.get(
-        FINVIZ_STOCK_URL,
-        {"t": ticker},
-        headers={"User-Agent": CHROME_USER_AGENT},
-    )
 
-    page = BeautifulSoup(response.content, "lxml")
+    page = parse_stock_page(ticker)
     tags_container = page.select_one(".quote-links div")
     if not tags_container:
-      raise ElementNotFoundError("Tags container not found")
+        raise ElementNotFoundError("Tags container not found")
 
     tags = tags_container.select("a")
     if len(tags) == 0:
-      raise ElementNotFoundError("Tags not found")
-    
-    tag_links = [{'Name': tag.text, 'Link': f"{FINVIZ_BASE_URL}{tag.get("href")}"} for tag in tags]
+        raise ElementNotFoundError("Tags not found")
+
+    tag_links = [
+        {"Name": tag.text, "Link": f"{FINVIZ_BASE_URL}{tag.get("href")}"}
+        for tag in tags
+    ]
     return pd.DataFrame(tag_links)
+
+
+def get_partial_metainfo_finviz(ticker: str) -> pd.DataFrame:
+    """
+    Gets partial metainfo for stock using finviz.
+
+    Args:
+        ticker (str): stock ticker symbol
+
+    Returns:
+        pd.DataFrame: pandas DataFrame of partial metainfo
+    """
+
+    page = parse_stock_page(ticker)
+    metainfo_table_elem = page.select_one(".js-snapshot-table")
+    if not metainfo_table_elem:
+        raise ElementNotFoundError("Metainfo table not found")
+
+    metainfo_table_cells = metainfo_table_elem.select("td")
+
+    target_labels = {
+        "Index": {
+            "name": "Index Participation",
+            "callback": lambda s: ",".join([name.strip() for name in s.split(",")]),
+        },
+        "EPS Y/Y TTM": {
+            "name": "EPS Yearly Growth",
+            "callback": lambda s: float(s[:-1]) / 100,
+        },
+        "EPS Q/Q": {
+            "name": "EPS Quarterly Growth",
+            "callback": lambda s: float(s[:-1]) / 100,
+        },
+        "EPS Surprise": {
+            "name": "EPS Surprise",
+            "callback": lambda s: float(s[:-1]) / 100,
+        },
+    }
+
+    metainfo_dict = {}
+    for i in range(0, len(metainfo_table_cells), 2):
+        label = metainfo_table_cells[i].text
+        if label in target_labels:
+            key = target_labels[label]["name"]
+            callback = target_labels[label]["callback"]
+            metainfo_dict[key] = callback(metainfo_table_cells[i + 1].text)
+
+    return pd.DataFrame.from_dict(metainfo_dict, orient="index", columns=["Value"])
