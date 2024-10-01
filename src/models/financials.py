cc78170f5a93a6d26937b9ab62915bd5cf4119e3
tigerding <zhiyuanding01@gmail.com>
2024-10-01 00:25:21 -0400

    feat: stock metainfo pydantic model

--- src/models/financials.py
+++ src/models/financials.py
@@ -3,11 +3,11 @@ Financials APIs related models.
 
 Author: tigerding
 Email: zhiyuanding01@gmail.com
-Version: 0.1.2
+Version: 0.2.0
 """
 
 from enum import Enum
-from pydantic import BaseModel, Field, HttpUrl
+from pydantic import BaseModel, HttpUrl
 from datetime import datetime
 
 
@@ -38,3 +38,41 @@ class TagInfo(BaseModel):
 
     name: str
     link: HttpUrl
+
+
+class StockMetaInfo(BaseModel):
+    """
+    Stock meta info model.
+    """
+
+    # yahoo
+    ticker: str  # symbol
+    name: str  # longName
+    exchange: str  # exchange
+    summary: str  # longBusinessSummary
+    employees: int  # fullTimeEmployees
+    dividend_rate: float  # dividendRate
+    price_to_book: float  # priceToBook
+    pirce_to_earning_ttm: float  # trailingPE
+    eps_ttm: float  # trailingEps
+    market_cap: int  # marketCap
+    fiftytwo_week_low: float  # fiftyTwoWeekLow
+    fiftytwo_week_high: float  # fiftyTwoWeekHigh
+    shares_outstanding: int  # sharesOutstanding
+    revenue: int  # totalRevenue
+    ebitda: int  # ebitda
+    gross_margins: float  # grossMargins
+    operating_margins: float  # operatingMargins
+    net_profit_margins: float  # profitMargins
+
+    # finviz
+    index_partipation: list[str]  # Index
+    eps_yearly_growth: float  # EPS Y/Y TTM
+    eps_quarterly_growth: float  # EPS Q/Q
+    eps_surprise: float  # EPS Surprise
+
+    # get_calendar
+    earnings_date: datetime
+
+
+# todo: current_price?
